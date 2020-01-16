a1d4446e1cf9f588bd4ae916d5a8856deef65057
gamliel <33903288+GamlielChemla@users.noreply.github.com>
2020-01-16 18:20:56 +0200

    d
    d

--- test/src/App.js
+++ test/src/App.js
@@ -1,40 +1,19 @@
 import React from 'react';
 import './App.css';
-import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
-} from 'reactstrap';
+import HomePage from './HomePage';
+import NavBar from './NavBar';
+import { BrowserRouter, Route, Link, Redirect } from "react-router-dom";
 
-import axios from "axios";
+
 
 
 function App() {
-
-  getToJson =()=>{
-
-    axios.get('/jsonId').then(res=>{
-      console.log("res",res);
-      
-    })
-  }
-  
   return (
+    <div className="container-fluid">
+    <NavBar/>
     
-    
-    <div>
-    <center>
-      <Card>
-        <CardImg top width="10%" src="https://cdn.onecklace.com/products/1468/product_1468_1_730.jpeg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example t</CardText>
-          <Button on onClick={this.getToJson}>Button</Button>
-        </CardBody>
-      </Card>
-      </center>
     </div>
   );
-};
+}
 
 export default App;
